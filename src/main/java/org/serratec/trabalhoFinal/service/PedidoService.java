4b7a2fba8db0b8e1cba07ccb6c67aad421eda727
Hyago Guimarães <rosah3677@gmail.com>
2025-10-27 20:18:02 -0300

    Update PedidoService.java

--- src/main/java/org/serratec/trabalhoFinal/service/PedidoService.java
+++ src/main/java/org/serratec/trabalhoFinal/service/PedidoService.java
@@ -59,7 +59,6 @@ public class PedidoService {
 					.orElseThrow(() -> new NotFoundException("Produto não encontrado: " + itemDTO.getProdutoId()));
 
 			if (!estoqueService.verificarEstoque(p.getId(), itemDTO.getQuantidade())) {
-
 				throw new RuntimeException("Estoque insuficiente para o produto: " + p.getNome());
 			}
 
